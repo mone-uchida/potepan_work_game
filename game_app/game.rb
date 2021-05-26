cd909259592ef47c3e2b3f78a3a5fd9f9bbbba7e
EC2 Default User <ec2-user@ip-172-31-42-147.ec2.internal>
2021-05-26 11:25:07 +0000

    initial commit

---
+++ game_app/game.rb
@@ -0,0 +1,65 @@
+puts "ジャンケン！"
+def  game_one
+ puts "0(グー)1(チョキ)2(パー)3(ジャンケンをやめる)"
+ player_number = gets.to_i
+ computer_number = rand(3)
+ if player_number == 3
+  puts "また遊んでね♪"
+  exit
+ end
+ shapes = ["グー","チョキ","パー"]
+ puts "ポン！"
+ puts "--------------------"
+ puts "あなたは#{shapes[player_number]}を出しました"
+ puts "相手は#{shapes[computer_number]}を出しました"
+ puts "--------------------"
+ 
+ if  player_number == computer_number
+ puts "あいこで"
+   return game_one
+ elsif (player_number == 0 && computer_number == 1)||(player_number == 1 && computer_number == 2)||(player_number == 2 && computer_number == 0)
+  return  game_two
+   #ジャンケン：プレイヤーの勝ち
+ else 
+  return game_three
+   #ジャンケン：プレイヤーの負け
+ end 
+end
+ 
+def game_two
+ puts "あっち向いて"
+ puts "0(上)1(下)2(右)3(左)"
+ player_number = gets.to_i
+ computer_number = rand(3)
+ directions = ["上","下","右","左"]
+ puts "ほい！"
+ puts "あなた:#{directions[player_number]}"
+ puts "相手:#{directions[computer_number]}"
+ puts "--------------------"
+ if player_number == computer_number
+ puts "☆あなたの勝ちです☆"
+ else
+   puts "ジャンケン！"
+   return game_one
+ end
+end
+
+def game_three
+ puts "あっち向いて"
+ puts "0(上)1(下)2(右)3(左)"
+ player_number = gets.to_i
+ computer_number = rand(3)
+ directions = ["上","下","右","左"]
+ puts "ほい！"
+ puts "あなた:#{directions[player_number]}"
+ puts "相手:#{directions[computer_number]}"
+ puts "--------------------"
+ if player_number == computer_number
+ puts "★あなたの負けです★"
+ else
+ puts "ジャンケン！"
+ return game_one
+ end
+end
+
+game_one
